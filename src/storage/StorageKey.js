b2d51169c52568a115eef8ef2e24017397909a87
thaikhuong62000 <thaikhuong62000@gmail.com>
2021-12-04 10:53:54 +0700

    Login Backend

---
+++ src/storage/StorageKey.js
@@ -0,0 +1,4 @@
+// let user = {
+//   jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYTk4ZDRjODM1ODU0MDAxNmZiYjYwZiIsImlhdCI6MTYzODU4NDU4NSwiZXhwIjoxNjQxMTc2NTg1fQ.O1b_0vM5TGtfyyMJgyU_DB7e8eI3lebFcb1x4Bn0oqQ",
+// };
+export const USER_KEY = "@user";
